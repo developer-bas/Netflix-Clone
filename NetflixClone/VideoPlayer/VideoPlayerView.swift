5da62c85b19a5801946a04fcedb4ceeaf03692bb
Sebastian <cruzguzmaneric@gmail.com>
2020-11-24 23:31:33 -0600

    VideoPlayerView Added

---
+++ NetflixClone/VideoPlayer/VideoPlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoPlayerView.swift
+//  NetflixClone
+//
+//  Created by PROGRAMAR on 24/11/20.
+//
+
+import SwiftUI
+
+struct VideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoPlayerView()
+    }
+}
