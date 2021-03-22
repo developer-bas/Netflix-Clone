ef7a00cba83553c4192364c75952104263b34826
Eric Sebastian <cruzguzmaneric@gmail.com>
2021-03-21 18:48:24 -0600

    Trailer player view

---
+++ NetflixClone/Preview/TrailerPlayerView.swift
@@ -0,0 +1,28 @@
+//
+//  TrailerPlayerView.swift
+//  NetflixClone
+//
+//  Created by PROGRAMAR on 21/03/21.
+//
+
+import SwiftUI
+import VideoPlayer
+
+struct TrailerPlayerView: View {
+    var videoURL : URL?
+    @Binding var playVideo : Bool
+    
+    var body: some View {
+        if videoURL != nil {
+            VideoPlayer(url: videoURL!, play: $playVideo)
+        } else{
+            Text("Could not load video")
+        }
+    }
+}
+
+struct TrailerPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrailerPlayerView(videoURL: nil, playVideo: .constant(true))
+    }
+}
